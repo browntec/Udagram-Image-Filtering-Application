8050461719d98affedf89e2b7e1806fd54a65c04
kloudmaster <brown4gold@gmail.com>
2022-07-23 18:03:56 +0100

    updates

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a16958a0c2a944b94a651549c214f671-1055469840.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a30c723dad8274ddc84c3fdc2d36f194-1628686978.us-east-1.elb.amazonaws.com /api/v0'
 };
 
 /*
